972972608125dcfa2bc81075f592de803d9e8ba4
Jino Jossy <jino.jossy@factset.com>
2022-03-14 17:16:00 +0530

    added gitignore

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 from common import helper
 
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__,
+            template_folder='template',
+            static_url_path='',
+            static_folder='links')
 
 @app.route('/')
 def index():
@@ -12,7 +15,12 @@ def index():
 def upload_file():
     uploaded_file = request.files['file']
     filepath = "output/{}".format(uploaded_file.filename)
+    link = "{}".format(uploaded_file.filename)
     if uploaded_file.filename != '':
         uploaded_file.save(filepath)
-        helper.build_symlink(filepath, uploaded_file.filename)
-    return redirect(url_for('index'))
+        helper.build_symlink(filepath, link)
+    return redirect(url_for('/{}'.format(link)))
+
+@app.route('/<path:filename>', methods=['GET'])
+def fetch(filename):
+    return send_from_directory(filename)
--- common/helper.py
+++ common/helper.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 def build_symlink(source, target):
-    p = Path(target)
     parent_dir = Path(__file__).parent.parent.absolute()
+    print("{}/{}".format(parent_dir, target))
+    print("{}/{}".format(parent_dir, source))
+    p = Path("{}/{}".format(parent_dir, target))
     p.symlink_to("{}/{}".format(parent_dir, source))
-
